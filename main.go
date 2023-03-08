ea9f3f8cc7d343b9067897dadcc5cb83422d0d16
alex jenkins server <alexandre@domont.org>
2023-03-08 16:30:48 +0100

    one

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+
+               "fmt"
+               "os"
+               "github.com/joho/godotenv"
+
+)
+
+func main() {
+
+               // Charger les variables d'environnement à partir du fichier .env
+
+               err := godotenv.Load()
+
+               if err != nil {
+
+                              fmt.Println("Erreur lors du chargement des variables d'environnement")
+
+               }
+
+               // Accéder aux variables d'environnement chargées
+
+               myVar := os.Getenv("MY_VAR")
+
+               fmt.Println(myVar)
+
+}
+
+
+// Fonction de test unitaire
+
+func TestMain(m *testing.M) {
+
+               os.Setenv("MY_VAR", "valeur_test")
+
+               retCode := m.Run()
+
+               os.Exit(retCode)
+
+}
